71d1aa5eb516950c2e4f2bd2c2f7013a6e1ceb9d
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2024-01-22 14:16:07 +1000

    New version with options

--- init.sh
+++ init.sh
@@ -11,22 +11,53 @@ EXC_FILES="README.md"
 BRANCH=master
 UPDATE_SUBM=false
 
+function parse_url {
+    echo ${1} | sed -n -E "s%^(([^:/@.]*://|)([^@/]+@|)([^:/@]+)[:/]([^:/@.][^:@]*)|)(@([^:@.]*)|)\$%\\${2}%p"
+}
+
+function check_url {
+    if test -z "$2"; then
+        echo "no url provided for $1" >&2
+        exit -1
+    fi
+    if test "$2" != "$(parse_url "$2" "0")"; then
+        echo "failed to parse url for $1: $2" >&2
+        exit -1
+    fi
+}
+
+function check_match {
+    if test -n "$4" && test "$3" != "$4"; then
+        echo "$1 $2 is different than the one specified:" >&2
+        echo " - $2 in the git repo: $3" >&2
+        echo " - $2 wanted: $4" >&2
+        exit 1;
+    fi
+}
+
 while test -n "$1"; do
 	case "$1" in
 	--submodules)
         UPDATE_SUBM=true
         ;;
-	-o|--overlay-remote)
+	-o|--over|--overlay|--overlay-remote)
         shift
-        WANT_URL_OVER="$1"
+        check_url "overlay" "$1"
+        WANT_BRANCH_OVER="$(parse_url "$1" "7")"
+        WANT_URL_OVER="$(parse_url "$1" "1")"
         ;;
-	-t|--tclb-remote)
+	-t|--tclb|--tclb-remote)
         shift
-        WANT_URL_TCLB="$1"
+        check_url "tclb" "$1"
+        WANT_BRANCH_TCLB="$(parse_url "$1" "7")"
+        WANT_URL_TCLB="$(parse_url "$1" "1")"
         ;;
     esac
     shift
 done
+
+echo "wanted: $WANT_URL_OVER @ $WANT_BRANCH_OVER"
+echo "wanted: $WANT_URL_TCLB @ $WANT_BRANCH_TCLB"
 
 mkdir -p .tclb
 
@@ -82,34 +113,81 @@ function gittclb {
 }
 
 URL_OVER="$(gitover remote get-url origin 2>/dev/null || true)"
+
+if test -z "$URL_OVER"; then
+    if test -n "$WANT_URL_OVER"; then
+        URL_OVER="$WANT_URL_OVER"
+        gitover remote add origin $URL_OVER
+    fi
+else
+    check_match "overlay" "remote" "$URL_OVER" "$WANT_URL_OVER"
+fi
+gitover fetch origin
+
+BRANCH_OVER="$(gitover branch --show-current 2>/dev/null || true)"
+
+if test -z "$BRANCH_OVER"; then
+    if test -n "$WANT_BRANCH_OVER"; then
+        BRANCH_OVER="$WANT_BRANCH_OVER"
+    else
+        BRANCH_OVER="master"
+    fi
+    gitover pull origin $BRANCH_OVER || true
+else
+    check_match "overlay" "branch" "$BRANCH_OVER" "$WANT_BRANCH_OVER"
+fi
+
+EXC_TMP_DIR=.tclb/tmp
+EXC_SAVED=""
+for i in $EXC_FILES; do
+    if test -f "$i"; then
+        mkdir -p "$EXC_TMP_DIR"
+        mv "$i" "$EXC_TMP_DIR"
+        EXC_SAVED="$EXC_SAVED $i"
+    fi
+done
+
 URL_TCLB="$(gittclb remote get-url origin 2>/dev/null || true)"
 
 if test -z "$URL_TCLB"; then
-    REMOTE="https://github.com/"
-    CREMOTE="$URL_OVER"
-    if ! test -z "$CREMOTE"; then
-        case "$CREMOTE" in git@github.com*)
-            REMOTE="git@github.com:"
+    if ! test -z "$WANT_URL_TCLB"; then
+        TCLB_OVER="$WANT_URL_TCLB"
+    else
+        case "$URL_OVER" in
+        git@github.com*)
+            URL_TCLB="git@github.com:CFD-GO/TCLB.git"
+            ;;
+        *)
+            URL_TCLB="https://github.com/CFD-GO/TCLB.git"
+            ;;
         esac
     fi
-    REMOTE="${REMOTE}CFD-GO/TCLB.git"
-    URL_TCLB="$REMOTE"
     gittclb remote add origin $URL_TCLB
-    EXC_TMP_DIR=.tclb/tmp
-    EXC_SAVED=""
-    for i in $EXC_FILES; do
-        if test -f "$i"; then
-            mkdir -p "$EXC_TMP_DIR"
-            mv "$i" "$EXC_TMP_DIR"
-            EXC_SAVED="$EXC_SAVED $i"
-        fi
-    done
-    gittclb pull origin master
+else
+    check_match "tclb" "remote" "$URL_TCLB" "$WANT_URL_TCLB"
+fi
+gitover fetch origin
+
+BRANCH_TCLB="$(gittclb branch --show-current 2>/dev/null || true)"
+
+if test -z "$BRANCH_TCLB"; then
+    if test -n "$WANT_BRANCH_TCLB"; then
+        BRANCH_TCLB="$WANT_BRANCH_TCLB"
+    else
+        BRANCH_TCLB="master"
+    fi
+    gittclb pull origin $BRANCH_TCLB
+else
+    check_match "tclb" "branch" "$BRANCH_TCLB" "$WANT_BRANCH_TCLB"
 fi
 
-echo "remotes:"
-echo "  tclb: $URL_TCLB"
-echo "  overlay: $URL_OVER"
+echo "repos:"
+echo "  tclb:"
+echo "    remote: $URL_TCLB"
+echo "    branch: $BRANCH_TCLB"
+echo "  overlay:"
+echo "    remote: $URL_OVER"
+echo "    branch: $BRANCH_OVER"
 
 for i in $EXC_FILES; do
     gittclb update-index --assume-unchanged "$i"
